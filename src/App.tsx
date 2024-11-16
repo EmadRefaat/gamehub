da0401f452923e564a96dd3e247e360bdaab306b
unknown <emad.refaat57@gmail.com>
2024-11-16 15:09:13 +0200

    make layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show, Text } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button>om </Button>
+      <Grid
+        gridTemplateAreas={{
+          base: `"nav nav" "main main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem bg={"red"} area={"nav"}>
+          navbar
+        </GridItem>
+
+        <GridItem
+          display={{ base: "none", lg: "block" }}
+          bg={"green"}
+          area={"aside"}
+        >
+          Sidebar
+        </GridItem>
+
+        <GridItem bg={"blue"} area={"main"}>
+          main{" "}
+        </GridItem>
+      </Grid>
     </>
   );
 }
