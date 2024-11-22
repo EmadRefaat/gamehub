2ccc951c8e2f5ac0bdddbe0df62c66206de8f4cb
emad refaat <emad.refaat57@gmial.com>
2024-11-22 19:58:42 +0200

    create a custom hook to fetch the games

--- src/Components/Gamegrid.tsx
+++ src/Components/Gamegrid.tsx
@@ -1,28 +1,8 @@
-import axios from "axios";
-import apiClient from "../services/api-client";
-import { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
-
-interface game {
-  id: number;
-  name: string;
-}
-
-interface gamesData {
-  count: number;
-  results: game[];
-}
+import useGames from "../Hooks/usegames";
 
 const Gamegrid = () => {
-  const [games, setGames] = useState<game[]>();
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<gamesData>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
