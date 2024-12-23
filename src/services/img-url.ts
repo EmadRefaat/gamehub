f6616c660f181efcf66f72a1a6c79387a95fee3a
emad refaat <emad.refaat57@gmial.com>
2024-12-23 09:53:19 +0200

    Handle games without image

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,5 +1,7 @@
+import noimage from "../assets/no_image.jpg";
+
 const getCropedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noimage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
